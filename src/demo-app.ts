f2da148e6518e90948c5e5f187fc0576bb8506c4
David Clark <>
2021-03-27 17:06:20 -0700

    Further simplify the repro example

--- src/demo-app.ts
+++ src/demo-app.ts
@@ -1,3 +1,6 @@
-import { DemoApp } from './DemoApp.js';
-
-customElements.define('demo-app', DemoApp);
+function helloWorld() {
+  const imgSrc = new URL("../../assets/tick-icon.png", import.meta.url);
+  const { href } = imgSrc;
+  console.log(href);
+}
+helloWorld();
